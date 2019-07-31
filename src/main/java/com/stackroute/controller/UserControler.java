8c0c1056b7c0a7d911bd8d769ee72a4784566d57
samaresh.maiti <samaresh.maiti@cgi.com>
2019-07-31 09:51:08 +0530

    update in controller class

--- src/main/java/com/stackroute/controller/UserControler.java
+++ src/main/java/com/stackroute/controller/UserControler.java
@@ -3,13 +3,16 @@ package com.stackroute.controller;
 import com.stackroute.module.User;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 public class UserControler {
-    @RequestMapping(value = "/")
-    public String message(ModelMap modelMap){
-        User user=new User();
-        user.setName("hey");
-        modelMap.addAttribute("message",user.getName());
-        return "welcome";
+    @RequestMapping("/")
+    public ModelAndView sendMessage() {
+        com.stackroute.module.User user= new User();
+        user.setName("Samaresh");
+        ModelAndView modelAndView= new ModelAndView("index");
+        modelAndView.addObject("message",user.getName()+" Welcome to Stackroute!!!");
+        return modelAndView;
     }
-}
+    }
+
